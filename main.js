39d0fb7065265160455b4a185c4718ae39f956da
Brayden <brownb5@oregonstate.edu>
2019-10-09 15:24:32 -0700

    Added colors to main js

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ color[1] = "#008080";
 color[2] = "#4169E1";
 color[3] = "#483D8B";
 color[4] = "#FF00FF";
+color[5] = "#90A2FF";
+color[6] = "#17FF92";
+color[7] = "#B16F6F";
+color[8] = "#D9F4EC";
+color[9] = "#21FDBB";
+color[10] = "#D221FD ";
 
 function changeColor()
 {
