ac066cebf63d507a4b2c267f53c3f4aca59c3a08
Brayden <brownb5@oregonstate.edu>
2019-10-09 15:19:57 -0700

    Created an onload function that  changes the background color when page refreshed Exercise 5

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+var color = new Array();
+color[0] = "#5ab4fd";
+color[1] = "#008080";
+color[2] = "#4169E1";
+color[3] = "#483D8B";
+color[4] = "#FF00FF";
+
+function changeColor()
+{
+  var randomColor = Math.floor(Math.random() * color.length);
+  console.log(color[randomColor]);
+    document.getElementsByTagName("body")[0].style.backgroundColor=color[randomColor];
+};
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("secondHeader").onclick = function() {
       this.style.color = 'green'
