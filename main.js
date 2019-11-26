7954264a49a830fbbc185997d6c244758ed32354
Brayden <brownb5@oregonstate.edu>
2019-11-25 16:35:51 -0800

    Made some spelling changes and changed some of the background colors when loaded

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var color = new Array();
 color[0] = "#5ab4fd";
 color[1] = "#216efd";
-color[2] = "#7afa83";
+color[2] = "#51ad58";
 color[3] = "#60d1ad";
 color[4] = "#c72e2e";
-color[5] = "#bd6be3";
+color[5] = "#8342a1";
 color[6] = "#B16F6F";
 
 function changeColor()
